586bc8dd8ddda9ba94ab9d726e5db70c1c6ef244
markfeighery <markfeighery@welfare.ie>
2018-12-06 11:30:52 +0000

    DRAFT

---
+++ iCanDoIT/iCanDoIT/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(iCanDoIT.Startup))]
+namespace iCanDoIT
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
